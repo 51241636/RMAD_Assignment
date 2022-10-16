f62e210f3e20308932cc471b332be5f490742f9d
Pasindu Induvara <hlsepala@gmail.com>
2022-10-16 11:42:43 +0530

    change example

--- src/Assignment.js
+++ src/Assignment.js
@@ -16,6 +16,8 @@ const Assignment = () => {
     } else {
       console.log('Fruit 3', 'Not Available');
     }
+    //ternary operator
+
 
     //Ternary Operator
     // name === 'Orange'
@@ -33,17 +35,39 @@ const Assignment = () => {
     //     break;
     // }
 
-    const marks = 90;
-    if (marks >= 75) {
-      console.log('your result is A');
-    } else if (marks >= 65) {
-      console.log('your result is B');
-    } else if (marks >= 55) {
-      console.log('your result is c');
-    } else if (marks >= 35) {
-      console.log('your result is S');
-    } else {
-      console.log('you fail the exam');
+   const marks =54;
+    // if (marks >= 75) {
+    //   console.log('your result is A');
+    // } else if (marks >= 65) {
+    //   console.log('your result is B');
+    // } else if (marks >= 55) {
+    //   console.log('your result is c');
+    // } else if (marks >= 35) {
+    //   console.log('your result is S');
+    // } else {
+    //   console.log('you fail the exam');
+    // }
+    // marks >=75
+    // ?console.log('your result is A')
+    // : marks >= 65
+    // ? console.log('your result is B')
+    // : marks >= 55
+    // ? console.log('your result is C')
+    // : marks >= 35
+    // ? console.log('your result is S')
+    // : console.log('your fail the exam');
+    switch (true){
+        case marks>=75:
+           console.log("your result is A") ;
+           break;
+           case marks>=65:
+           console.log("your result is B") ;
+           break;
+           case marks>=55:
+           console.log("your result is C") ;
+           break;
+           default:
+           console.log("your result is S") ;
     }
   };
 
