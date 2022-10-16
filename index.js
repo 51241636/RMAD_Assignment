c3de28cdcbbfe3a2e15297f6055a95c280db01a7
Pasindu Induvara <hlsepala@gmail.com>
2022-10-16 17:04:15 +0530

    array fillter

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import Assignment from './src/Assignment';
+import lesson2 from './src/lesson2';
 
 
 
 
-AppRegistry.registerComponent(appName, () =>Assignment);
+AppRegistry.registerComponent(appName, () =>lesson2);
