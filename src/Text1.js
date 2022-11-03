6cd41ed786a3b202b89b5e5ac57b850435a01a84
Pasindu Induvara <hlsepala@gmail.com>
2022-11-03 13:34:59 +0530

    login

--- src/Text1.js
+++ src/Text1.js
@@ -1,17 +1,36 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Text, StyleSheet, SafeAreaView, TouchableOpacity, Button } from 'react-native';
+import { Text, StyleSheet, SafeAreaView, TouchableOpacity, Button, TextInput } from 'react-native';
 import Lesson2 from './Lesson2';
 
-const Text1 = () => {
-  const navigation=useNavigation();
+const Text1 = ({navigation}) => {
+ 
+    const [name, setName] = React.useState("");
+    const [mail, setMail] = React.useState(null);
+    
   
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}> this is text 1</Text>
+      <Text style={styles.title}> Login details</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="enter your name?"
+        onChangeText={val => setName(val)}
+        value={name}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setMail}
+        value={mail}
+        placeholder="enter your email address?"
+        keyboardType="text"
+      />
       <TouchableOpacity 
-    onPress={() => navigation.push('Lesson2')}>
-      <Text style={styles.bus}> Go to Lesson2</Text>
+    onPress={() => navigation.push('Lesson2', {
+      name:"Pasindu",
+
+    })}>
+      <Text style={styles.bus}> Submit</Text>
       
   
 
@@ -25,7 +44,7 @@ const styles = StyleSheet.create({
 flex:1,
 justifyContent:"center",
 alignItems:"center",
-backgroundColor:"yellow"
+backgroundColor:"black"
   },
   title:{
     fontSize:20,
@@ -35,6 +54,15 @@ backgroundColor:"yellow"
     backgroundColor:"blue",
     fontSize:18,
    
+  },
+  input:{
+    alignSelf:"center",
+width:300,
+height:50,
+color:"yellow",
+margin: 12,
+    borderWidth: 1,
+    padding: 10,
   }
 });
 
